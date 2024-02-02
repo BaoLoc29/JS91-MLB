43349a4cc6c39b4b610e277405a75ad16f4916c3
loctc <loctcu@gmail.com>
2024-02-02 20:14:08 +0700

    comp products + user

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,7 +6,7 @@ const Home = () => {
             <div className='banner-home'>
                 <img src="https://file.hstatic.net/200000642007/file/mlb_1920x640_fe32c1742f8c416e9454a950210f885b.jpg" alt="banner" className='banner'></img>
             </div>
-            <div className='container'>
+            <div className='product-list'>
                 <Products />
             </div>
         </div>
